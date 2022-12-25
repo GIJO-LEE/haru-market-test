13540ffa078b81b693fb44780db4c5bce24bc620
Heon <gijoo0104@gmail.com>
2022-12-25 22:33:09 +0900

    modified post_detail_page.dart to load Firebase DB post and order, but still in progress.

--- lib/post_detail_page.dart
+++ lib/post_detail_page.dart
@@ -42,8 +42,6 @@ class PostDetailPage extends StatefulWidget {
 
 class _PostDetailPageState extends State<PostDetailPage> {
   DateTime register_time = DateTime.parse('2022-11-18 21:50:00');
-  Post post = Post("10/20에 구매한 아이폰 13 Pro Max 팔아요", "상품상세설명1", "강남구 신사동", "삼성");
-  Seller seller = Seller("스파르타", "https://i.ibb.co/CwzHq4z/trans-logo-512.png");
 
   String calculateRemainTime(DateTime register_time) {
     Duration remain_time = register_time.difference(DateTime.now());
@@ -94,155 +92,282 @@ class _PostDetailPageState extends State<PostDetailPage> {
           ),
         ],
       ),
-      body: FutureBuilder<DocumentSnapshot>(
-        future: FirebaseFirestore.instance.collection("post").doc(postid).get(),
+      body: FutureBuilder<List<dynamic>>(
+        future: Future.wait([
+          FirebaseFirestore.instance.collection("post").doc(postid).get(),
+          FirebaseFirestore.instance
+              .collection("order")
+              .where("postId", isEqualTo: postid)
+              .get(),
+          FirebaseFirestore.instance.collection("user").get(),
+          FirebaseFirestore.instance
+              .collection("user")
+              .where("userId", isEqualTo: uid)
+              .get(),
+          // FirebaseFirestore.instance.collection("user").where("userId", isEqualTo: ).get(),
+        ]),
         builder: (context, snapshot) {
-          final post = snapshot.data;
+          final post = snapshot.data?[0];
+          final order = snapshot.data?[1];
+          final users = snapshot.data?[2];
+          final customer = snapshot.data?[3];
+          // final seller = snapshot.data?[4];
           if (post == null) {
             return Text('no data');
           }
-          return Column(
-            children: [
-              ////// 아이템 이미지 롤링
-              AspectRatio(
-                aspectRatio: 12 / 4,
-                child: ItemImgaeViewWidget(),
-              ),
-              ////// 판매자 정보
-              Card(
-                child: ListTile(
-                  // 판매자 정보 : 프로필 사진
-                  leading: CircleAvatar(
-                    radius: 36,
-                    backgroundColor: Colors.white,
-                    // Image를 원 안에 제대로 들어가게 하기 위해 Padding으로 Wrapping!! (Good)
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Image.network(
-                        seller.profileImage,
-                        width: 62,
+          final temp_img_uri = "https://i.ibb.co/CwzHq4z/trans-logo-512.png";
+
+          print("post : ${post}");
+          print("order : ${order}");
+          print("users : ${users}");
+          return SingleChildScrollView(
+            child: Column(
+              children: [
+                ////// 아이템 이미지 롤링
+                AspectRatio(
+                  aspectRatio: 12 / 4,
+                  child: ItemImgaeViewWidget(),
+                ),
+                ////// 판매자 정보
+                Card(
+                  child: ListTile(
+                    // 판매자 정보 : 프로필 사진
+                    leading: CircleAvatar(
+                      radius: 36,
+                      backgroundColor: Colors.white,
+                      // Image를 원 안에 제대로 들어가게 하기 위해 Padding으로 Wrapping!! (Good)
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Image.network(
+                          temp_img_uri,
+                          width: 62,
+                        ),
                       ),
                     ),
-                  ),
-                  // 판매자 정보 : 닉네임
-                  title: Text(seller.nickName),
-                  // 판매 위치 정보
-                  subtitle: Text(post.get('location')),
-                  // 판매 시작
-                  trailing: TimerBuilder.periodic(
-                    const Duration(seconds: 1),
-                    builder: (context) {
-                      // 함수 작성 후 return Text에 넣기!!
-                      return Text(
-                        calculateRemainTime(register_time),
-                        style: const TextStyle(
-                          fontSize: 12,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      );
-                    },
-                  ),
-                ),
-              ),
-              ////// 판매상품정보
-              // 제목
-              Container(
-                width: double.infinity,
-                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                child: Text(
-                  post.get('articleTitle'),
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
+                    // 판매자 정보 : 닉네임
+                    title: Text("닉네임 데터 가져오기"), // seller.get("nickName")
+                    // 판매 위치 정보
+                    subtitle: Text(post.get('location')),
+                    // 판매 시작
+                    trailing: TimerBuilder.periodic(
+                      const Duration(seconds: 1),
+                      builder: (context) {
+                        // 함수 작성 후 return Text에 넣기!!
+                        return Text(
+                          calculateRemainTime(
+                              post.get("auctionEndDateTime").toDate()),
+                          style: const TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        );
+                      },
+                    ),
                   ),
                 ),
-              ),
-              // 상세정보 텍스트
-              Container(
-                width: double.infinity,
-                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                child: Text(
-                  '상세정보',
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              // 상세정보 데이터 테이블
-              Container(
-                width: double.infinity,
-                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                child: Table(
-                  border: TableBorder.all(color: Colors.transparent),
-                  columnWidths: const <int, TableColumnWidth>{
-                    0: FixedColumnWidth(50),
-                    1: FlexColumnWidth(),
-                    // FlexColumnWidth(), IntrinsicColumnWidth()
-                  },
-                  defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-                  children: <TableRow>[
-                    TableRow(
-                      children: <Widget>[
-                        Container(
-                          padding: EdgeInsets.symmetric(vertical: 5),
-                          child: Text('제조사'),
-                        ),
-                        Text(post.get('manufacturingCompany'))
-                      ],
-                    ),
-                    TableRow(
-                      children: <Widget>[
-                        Container(
-                          padding: EdgeInsets.symmetric(vertical: 5),
-                          child: Text('시리즈'),
-                        ),
-                        Text(post.get('series'))
-                      ],
-                    ),
-                    TableRow(
-                      children: <Widget>[
-                        Container(
-                          padding: EdgeInsets.symmetric(vertical: 5),
-                          child: Text('모델명'),
-                        ),
-                        Text(post.get('modelName'))
-                      ],
-                    ),
-                    TableRow(
-                      children: <Widget>[
-                        Container(
-                            padding: EdgeInsets.symmetric(vertical: 5),
-                            child: Text('용량')),
-                        Text('512GB(데이터X)')
-                      ],
-                    ),
-                    TableRow(
-                      children: <Widget>[
-                        Container(
-                            padding: EdgeInsets.symmetric(vertical: 5),
-                            child: Text('색상')),
-                        Text('검정(데이터X)')
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                child: SizedBox(
+                ////// 판매상품정보
+                // 제목
+                Container(
                   width: double.infinity,
+                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
                   child: Text(
-                    post.get('description'),
-                    maxLines: 4,
-                    overflow: TextOverflow.ellipsis,
+                    post.get('articleTitle'),
                     textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-              ),
-            ],
+                // 상세정보 텍스트
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  child: Text(
+                    '상세정보',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                // 상세정보 데이터 테이블
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  child: Table(
+                    border: TableBorder.all(color: Colors.transparent),
+                    columnWidths: const <int, TableColumnWidth>{
+                      0: FixedColumnWidth(50),
+                      1: FlexColumnWidth(),
+                      // FlexColumnWidth(), IntrinsicColumnWidth()
+                    },
+                    defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+                    children: <TableRow>[
+                      TableRow(
+                        children: <Widget>[
+                          Container(
+                            padding: EdgeInsets.symmetric(vertical: 5),
+                            child: Text('제조사'),
+                          ),
+                          Text(post.get('manufacturingCompany'))
+                        ],
+                      ),
+                      TableRow(
+                        children: <Widget>[
+                          Container(
+                            padding: EdgeInsets.symmetric(vertical: 5),
+                            child: Text('시리즈'),
+                          ),
+                          Text(post.get('series'))
+                        ],
+                      ),
+                      TableRow(
+                        children: <Widget>[
+                          Container(
+                            padding: EdgeInsets.symmetric(vertical: 5),
+                            child: Text('모델명'),
+                          ),
+                          Text(post.get('modelName'))
+                        ],
+                      ),
+                      TableRow(
+                        children: <Widget>[
+                          Container(
+                              padding: EdgeInsets.symmetric(vertical: 5),
+                              child: Text('용량')),
+                          Text('512GB(데이터X)')
+                        ],
+                      ),
+                      TableRow(
+                        children: <Widget>[
+                          Container(
+                              padding: EdgeInsets.symmetric(vertical: 5),
+                              child: Text('색상')),
+                          Text('검정(데이터X)')
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: Text(
+                      post.get('description'),
+                      maxLines: 4,
+                      overflow: TextOverflow.ellipsis,
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                ),
+                Divider(height: 10),
+                // 입찰자 리스트 텍스트
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  child: Text(
+                    '입찰자 리스트',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                // 입찰자 리스트 확인
+                order.docs.length == 0
+                    ? Center(child: Text("입찰자가 없습니다."))
+                    : Container(
+                        child: ListView.separated(
+                          padding: EdgeInsets.zero,
+                          scrollDirection: Axis.vertical,
+                          shrinkWrap: true,
+                          itemCount: order.docs.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return Container(
+                              alignment: Alignment.center,
+                              child: SizedBox(
+                                height: 100,
+                                width: double.infinity,
+                                child: ListTile(
+                                  contentPadding: EdgeInsets.only(
+                                      left: 20, top: 010, right: 0, bottom: 0),
+                                  // 판매리스트 이미지
+                                  // leading: Image.network(
+                                  //   "https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP848/iphone13-pro-max-colors-480_2x.png",
+                                  // ),
+                                  leading: ClipRRect(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(15)),
+                                    child: Image.network(
+                                      "https://images.unsplash.com/photo-1616348436168-de43ad0db179?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80",
+                                      alignment: Alignment.center,
+                                      height: 50,
+                                    ),
+                                  ),
+                                  // 판매리스트 제목
+                                  title:
+                                      Text(order.docs[index].get('orderUserId'),
+                                          style: TextStyle(
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.bold,
+                                          )),
+                                  // 판매리스트 위치
+                                  subtitle: Text(order.docs[index]
+                                      .get('orderPrice')
+                                      .toString()),
+                                ),
+                              ),
+                            );
+                          },
+                          separatorBuilder: (BuildContext context, int index) {
+                            return Divider(
+                              thickness: 1,
+                            );
+                          },
+                        ),
+                      ),
+                // ////// 판매자 정보
+                // Card(
+                //   child: ListTile(
+                //     // 판매자 정보 : 프로필 사진
+                //     leading: CircleAvatar(
+                //       radius: 36,
+                //       backgroundColor: Colors.white,
+                //       // Image를 원 안에 제대로 들어가게 하기 위해 Padding으로 Wrapping!! (Good)
+                //       child: Padding(
+                //         padding: const EdgeInsets.all(8.0),
+                //         child: Image.network(
+                //           temp_img_uri,
+                //           width: 62,
+                //         ),
+                //       ),
+                //     ),
+                //     // 판매자 정보 : 닉네임
+                //     title: Text(seller.nickName),
+                //     // 판매 위치 정보
+                //     subtitle: Text(post.get('location')),
+                //     // 판매 시작
+                //     trailing: TimerBuilder.periodic(
+                //       const Duration(seconds: 1),
+                //       builder: (context) {
+                //         // 함수 작성 후 return Text에 넣기!!
+                //         return Text(
+                //           calculateRemainTime(register_time),
+                //           style: const TextStyle(
+                //             fontSize: 12,
+                //             fontWeight: FontWeight.w600,
+                //           ),
+                //         );
+                //       },
+                //     ),
+                //   ),
+                // ),
+              ],
+            ),
           );
         },
       ),
